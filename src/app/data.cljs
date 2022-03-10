28d7727502c95c8574d0782d007eb51b083f88a1
Bryan Mikaelian <bryan@netlify.com>
2022-03-10 16:50:03 -0500

    Fix colors (#2)

--- src/app/data.cljs
+++ src/app/data.cljs
@@ -8,9 +8,9 @@
              {:color/name "Indian Yellow", :color/hex "#FFB800"}
              {:color/name "Midnight Black" , :color/hex "#000000"}
              {:color/name "Phthalo Blue" , :color/hex "#0C0040"}
-             {:color/name "Phthalo Green" , :color/hex "#102E3C"}
+             {:color/name "Phthalo Green" , :color/hex "#123524"}
              {:color/name "Prussian Blue" , :color/hex "#021E44"}
              {:color/name "Sap Green" , :color/hex "#0A3410"}
              {:color/name "Titanium White" , :color/hex "#FFFFFF"}
-             {:color/name "Van Dyke Brown" , :color/hex "#221B15"}
+             {:color/name "Van Dyke Brown" , :color/hex "#664228"}
              {:color/name "Yellow Ochre" , :color/hex "#C79B00"}])
