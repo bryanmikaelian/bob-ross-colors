dab2a82b21fe36fa2e2054bb7df1de856451721e
Bryan Mikaelian <bryan@netlify.com>
2022-03-10 15:55:04 -0500

    Render all the colors

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,13 +1,33 @@
 (ns app.core
-  (:require [reagent.dom :as rdom]))
+  (:require [reagent.dom :as rdom]
+            
+            [app.data :as d]))
+
+(defn- header-component
+  []
+  [:div.py-4
+   [:div.max-w-7xl.mx-auto.px-4
+    [:div.text-center
+     [:p.mt-2.text-3xl.leading-8.font-extrabold.tracking-tight.text-gray-900 "Bob Ross' Colors"]]]])
+
+(defn- color-swatch-component
+  [color]
+  (let [name (color :color/name)
+        hex (color :color/hex)]
+    [:li.col-span-1.flex.flex-col.text-center.bg-white.bounded-lg.shadow.p-10
+     [:div.w-32.h-32.flex-shrink-0.mx-auto.border {:style { :background-color hex}}]
+     [:p.text-gray-900.font-semibold.hover:text-gray-600.pt-5.uppercase name]
+     [:p.text-gray-900.font-medium.hover:text-gray-600 hex]]))
+
 
 (defn app
   []
-  [:div.py-12.bg-white
-   [:div.max-w-7xl.mx-auto.px-4
-    [:div.text-center
-     [:h2.text-base.text-indigo-600.font-semiboldy.tracking-wide.uppercase "Welcome"]
-     [:p.mt-2.text-3xl.leading-8.font-extrabold.tracking-tight.text-gray-900 "Hello, ClojureScript!"]]]])
+  [:div
+    [header-component]
+    [:div.max-w-7xl.mx-auto.px-4.pt-4.pb-8
+      [:ul.grid.grid-cols-2.gap-x-4.gap-y-8
+        (for [color d/colors]
+          ^{:key (color :color/hex)} (color-swatch-component color))]]])
 
 (defn start []
   (rdom/render [app] (. js/document (getElementById "app"))))
