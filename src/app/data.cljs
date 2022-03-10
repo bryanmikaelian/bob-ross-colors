dfb07ee1f36e5c1297fa3990c41f35a4a70207d0
Bryan Mikaelian <bryan@netlify.com>
2022-03-10 15:58:56 -0500

    Render all the colors (#1)

---
+++ src/app/data.cljs
@@ -0,0 +1,16 @@
+(ns app.data)
+
+
+(def colors [{:color/name "Alizarin Crimson", :color/hex "#4E1500"}
+             {:color/name "Bright Red", :color/hex "#DB0000"}
+             {:color/name "Cadium Yellow", :color/hex "#FFEC00"}
+             {:color/name "Dark Sienna", :color/hex "#5F2E1F"}
+             {:color/name "Indian Yellow", :color/hex "#FFB800"}
+             {:color/name "Midnight Black" , :color/hex "#000000"}
+             {:color/name "Phthalo Blue" , :color/hex "#0C0040"}
+             {:color/name "Phthalo Green" , :color/hex "#102E3C"}
+             {:color/name "Prussian Blue" , :color/hex "#021E44"}
+             {:color/name "Sap Green" , :color/hex "#0A3410"}
+             {:color/name "Titanium White" , :color/hex "#FFFFFF"}
+             {:color/name "Van Dyke Brown" , :color/hex "#221B15"}
+             {:color/name "Yellow Ochre" , :color/hex "#C79B00"}])
